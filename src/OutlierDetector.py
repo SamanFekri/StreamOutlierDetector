c0b9e82623bedf87f1eb0daaf1d92e247a08078a
skings <samanf74@gmail.com>
2019-07-06 17:07:20 +0430

    just add structure of class

---
+++ src/OutlierDetector.py
@@ -0,0 +1,12 @@
+class OutlierDetector:
+
+    def __init__(self, target_standard_deviation=2, size_current_sample=20, size_initial_ignore=10):
+        pass
+
+
+    def push(self, value):
+        pass
+
+
+    def push_async(self, value, result_callback):
+        pass
