829da162613ec43cc3948d99b5b491eb5eec41b7
skings <samanf74@gmail.com>
2019-07-06 17:09:30 +0430

    create constructor of class

--- src/OutlierDetector.py
+++ src/OutlierDetector.py
@@ -1,7 +1,11 @@
 class OutlierDetector:
 
     def __init__(self, target_standard_deviation=2, size_current_sample=20, size_initial_ignore=10):
-        pass
+        self.target_standard_deviation = target_standard_deviation
+        self.size_current_sample = size_current_sample
+        self.size_initial_ignore = size_initial_ignore
+        self.total_mean = 0
+
 
 
     def push(self, value):
