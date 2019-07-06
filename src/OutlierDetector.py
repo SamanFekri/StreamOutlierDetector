f22e67ee46f955fa3e752cebdefc155c0bf0ace3
skings <samanf74@gmail.com>
2019-07-06 17:12:16 +0430

    just count number of instance we create from this class

--- src/OutlierDetector.py
+++ src/OutlierDetector.py
@@ -1,10 +1,12 @@
 class OutlierDetector:
+    number_instances = 0
 
     def __init__(self, target_standard_deviation=2, size_current_sample=20, size_initial_ignore=10):
         self.target_standard_deviation = target_standard_deviation
         self.size_current_sample = size_current_sample
         self.size_initial_ignore = size_initial_ignore
         self.total_mean = 0
+        OutlierDetector.number_instances += 1
 
 
 
