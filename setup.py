fa61851b00e44575f8fbd65a68dc971e3eae45ec
skings <samanf74@gmail.com>
2019-07-09 12:29:40 +0430

    add another version with correct readme

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name='pyood',
     packages=['pyood'],
-    version='0.2',
+    version='0.2.1',
     scripts=['pyood/OutlierDetector.py'] ,
     author="Saman Fekri",
     author_email="samanf74@gmail.com",
